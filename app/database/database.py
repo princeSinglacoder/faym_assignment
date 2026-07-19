19a8d1e9ceff8b09849b142c156999ca504d058b
Prince Singla <princesingla8570@gmail.com>
2026-07-19 13:41:33 +0530

    add Database Deployed URL

--- app/database/database.py
+++ app/database/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:Admin@localhost:5432/faymDB"
+DATABASE_URL = "postgresql://faymdb_user:GcdlFF7iVU9WSEni72cNeBsLTyRffurq@dpg-d9e8b8741pts73eei6n0-a/faymdb"
+
 
 engine = create_engine(DATABASE_URL)
 
