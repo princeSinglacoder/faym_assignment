35ceea179c9b6e699c1391121e8a939036d2b67a
Prince Singla <princesingla8570@gmail.com>
2026-07-19 13:18:05 +0530

    add secret keys in env file

--- app/utils/jwt.py
+++ app/utils/jwt.py
@@ -2,10 +2,14 @@ import datetime
 from datetime import timedelta, timezone
 import bcrypt
 from jose import jwt, JWTError
+import os 
+from dotenv import load_dotenv
 
-SECRET_KEY = "faym-super-secret-key-for-jwt-affiliate-assignment-12345"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1440  # 24 hours
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 def hash_password(password: str) -> str:
     salt = bcrypt.gensalt()
@@ -23,7 +27,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     if expires_delta:
         expire = datetime.datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = datetime.datetime.now(timezone.utc) + timedelta(minutes=1440)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
